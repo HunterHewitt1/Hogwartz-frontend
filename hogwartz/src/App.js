82265c595bc783af578785b328b3e22a5347fc2c
John <jboyle15@yahoo.com>
2022-11-21 09:19:16 -0600

    working on components

--- hogwartz/src/App.js
+++ hogwartz/src/App.js
@@ -1,7 +1,11 @@
+import Nav from './components/Nav'
+import Home from './components/Home'
 function App() {
   return (
     <div className="App">
       <p>app.js hogwartz</p>
+      <Nav />
+      <Home />
     </div>
   )
 }
