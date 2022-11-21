7e3f5c6a9750af99ca944b3ad27773f3a67483f8
John <jboyle15@yahoo.com>
2022-11-21 10:48:50 -0600

    added some rputes and links

--- hogwartz/src/pages/SearchResults.js
+++ hogwartz/src/pages/SearchResults.js
@@ -1,17 +1,15 @@
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import {BASE_URL} from '../globals'
+import { BASE_URL } from '../globals'
 import { useEffect, useState } from 'react'
 import Student from '../components/Student'
 
-
-const SearchResults = ()=>{
-
-return(
+const SearchResults = () => {
+  return (
     <div>
-     
+      <h1>these are the search results</h1>
     </div>
-)
+  )
 }
 
-export default SearchResults;
+export default SearchResults
