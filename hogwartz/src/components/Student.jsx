66517cba7f73319d42ac9dd2055fe5f083258448
Shondriane <shondriane.wise@yahoo.com>
2022-11-21 10:52:09 -0500

    student component

--- hogwartz/src/components/Student.jsx
+++ hogwartz/src/components/Student.jsx
@@ -3,8 +3,19 @@ const Student = ()=>{
         <div>
             <div>
                 <h1>name</h1>
-                <
             </div>
+            <div>
+                <h2>house</h2>
+            </div>
+            <div>
+                <h2>courses</h2>
+            </div>
+            <div>
+                <h2> grades</h2>
+            </div>
+            <div>
+                <h3>gpa</h3>
+                 </div>
         </div>
     )
 }
